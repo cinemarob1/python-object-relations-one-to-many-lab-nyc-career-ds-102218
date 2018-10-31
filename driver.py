cbb2f65de6eaac4b0378deac8c2beecebe0dd063
cinemarob1 <cinemarob1@gmail.com>
2018-10-30 20:50:07 -0400

    Starting to get how this works

--- driver.py
+++ driver.py
@@ -1,12 +1,18 @@
-# remeber to import the trip class here
-
+from trip import Trip
 
 class Driver:
     def __init__(self, name):
-        self.name = name
+        self._name = name
 
+    @property
+    def name(self):
+        return self._name
+    
     def my_trips(self):
-        pass
+        my_trips = []
+        all_trips = Trip.all()
+        my_trips = [f'{trip.start} to {trip.destination}' for trip in all_trips if trip.driver.name == self.name]
+        return my_trips
 
     def my_trip_summaries(self):
         pass
--- owner.py
+++ owner.py
@@ -1,10 +1,29 @@
-# import car class here
-
-
 class Owner:
     def __init__(self, name, age):
         self._name = name
         self._age = age
 
     def find_my_cars(self):
-        pass
+        from car import Car
+        cars = Car.all()
+        my_cars = [f'{car.make} {car.model}' for car in cars if car.owner.name == self.name]
+        return my_cars
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        self._name = name
+    
+    @property
+    def age(self):
+        return self._age
+    
+    @age.setter
+    def age(self, age):
+        self._age = age
+
+
+############
